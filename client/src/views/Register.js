11f4ccc7399472d86a037bb85e33d62afa3cb1bd
Kacy Gilbert <gilbertkacy90@gmail.com>
2023-04-23 07:34:04 -0500

    DGray User Registration started

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const Register = () => {
+const Register = ({user}) => {
+  const [currentName, setCurrentName] = useState('');
+  const handleSubmit = () => {
+    user.name.current = currentName
+  }
   return (
     <div>
       <h1>Register</h1>
       <Link to='/home'>Home</Link>
+      <form onSubmit={(e)=>handleSubmit()}>
+        <label htmlFor='currentName'>User Name:</label>
+        <input type='text' id='currentName' name='currentName' required minLength='3' onChange={(e)=>setCurrentName(e.target.value)} />
+        <button>Change User Name</button>
+      </form>
     </div>
   )
 }
